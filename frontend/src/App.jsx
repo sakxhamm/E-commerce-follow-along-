9fd794db16aead09c71762671a7a222929720059
sakxhamm <saksham.gupta.63@kalvium.community>
2025-02-04 14:45:25 +0530

    createproduct added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter , Routes , Route} from "react-router-dom";
-import { LoginPage,SignupPage} from "./Routes.jsx";
+import { LoginPage,SignupPage, CreateProduct} from "./Routes.jsx";
 import './App.css' ;
 import Home from "./pages/Home.jsx";
 function App (){
   return (
     <BrowserRouter>
     <Routes>
+      
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/" element={<Home />} />
+      <Route path="/create-product" element={<CreateProduct />} />
       
     </Routes>
     </BrowserRouter>
--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,4 +1,6 @@
 import LoginPage from "./pages/login"
 import SignupPage from "./pages/signup"
+import Home from "./pages/Home";
+import CreateProduct from "./pages/createProduct.jsx";
 
-export {LoginPage, SignupPage} 
+export {LoginPage, SignupPage,CreateProduct,Home} 
